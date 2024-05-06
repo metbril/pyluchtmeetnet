45c81ec45bd8122ef93d7f28df4709503bedee06
Robert van Bregt <git@robertvanbregt.nl>
2024-05-06 14:21:01 +0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyluchtmeetnet",
-    version="0.1.2",
+    version="0.1.3",
     description="A module for getting air quality data from Luchtmeetnet OpenAPI",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,16 +25,16 @@ setup(
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="air quality, health, api",
     packages=find_packages(),
-    python_requires=">=3.7, <4",
+    python_requires=">=3.8, <4",
     install_requires=["requests"],
     project_urls={
         "Bug Reports": "https://github.com/metbril/pyluchtmeetnet/issues",
